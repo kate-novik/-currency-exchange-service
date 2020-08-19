9463f29e6d7d7d067e83d53f5f14cac4eb65bb57
Kate <ekat.novik@gmail.com>
2020-08-20 00:33:31 +0300

    Configure JPA and initialized data for exchange value.

--- src/main/java/com/novik/microservices/currencyexchangeservice/ExchangeValue.java
+++ src/main/java/com/novik/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,6 +1,9 @@
 package com.novik.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,10 +15,14 @@ import lombok.Setter;
 @NoArgsConstructor
 @Setter
 @Getter
+@Entity
 public class ExchangeValue {
 
+  @Id
   private Long id;
+  @Column(name = "currency_from")
   private String from;
+  @Column(name = "currency_to")
   private String to;
   private BigDecimal conversionMultiple;
 
