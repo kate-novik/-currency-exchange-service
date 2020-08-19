b4633dd772969b2099e64410d542698abb12af14
Kate <ekat.novik@gmail.com>
2020-08-19 23:49:57 +0300

    Add dynamic port view.

--- src/main/java/com/novik/microservices/currencyexchangeservice/ExchangeValue.java
+++ src/main/java/com/novik/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,15 +1,17 @@
 package com.novik.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * ExchangeValue DTO.
  * @author Kate Novik
  */
-@AllArgsConstructor
-@Data
+@NoArgsConstructor
+@Setter
+@Getter
 public class ExchangeValue {
 
   private Long id;
@@ -17,4 +19,13 @@ public class ExchangeValue {
   private String to;
   private BigDecimal conversionMultiple;
 
+  private int port;
+
+  public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+    this.id = id;
+    this.from = from;
+    this.to = to;
+    this.conversionMultiple = conversionMultiple;
+
+  }
 }
