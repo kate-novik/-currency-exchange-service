e2c1da2bf66b842a541eb8394852e3612f49fe76
Kate <ekat.novik@gmail.com>
2020-08-24 02:04:54 +0300

    Create ExchangeValueRepository, and inject it in controller

--- src/main/java/com/novik/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/novik/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,7 @@ package com.novik.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyExchangeController {
 
   @Autowired
-  private Environment environment;
+  private Environment env;
+
+  @Autowired
+  private ExchangeValueRepository exchangeValueRepository;
 
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-    ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
-    exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+    ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
+//        new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+
+    exchangeValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
     return exchangeValue;
   }
 
--- src/main/java/com/novik/microservices/currencyexchangeservice/ExchangeValueRepository.java
+++ src/main/java/com/novik/microservices/currencyexchangeservice/ExchangeValueRepository.java
@@ -1,8 +1,16 @@
-package com.novik.microservices.currencyexchangeservice;/**
- *
+package com.novik.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * ExchangeValueRepository.
  *
  * @author Kate Novik
  */
+@Repository
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
 
-public interface ExchangeValueRepository {
+  ExchangeValue findByFromAndTo(String from, String to);
+
 }
