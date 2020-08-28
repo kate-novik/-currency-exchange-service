060ac39ca7059b76dbcc68f3d55e97fdc437430d
Kate <ekat.novik@gmail.com>
2020-08-29 01:26:57 +0300

    Add Sleuth for adding unique id to all requests in microservice.

--- src/main/java/com/novik/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/novik/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,17 +1,17 @@
 package com.novik.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * CurrencyExchangeController.
  * @author Kate Novik
  */
+@Log4j2
 @RestController
 public class CurrencyExchangeController {
 
@@ -27,6 +27,7 @@ public class CurrencyExchangeController {
 //        new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
 
     exchangeValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+    log.info("{}", exchangeValue);
     return exchangeValue;
   }
 
